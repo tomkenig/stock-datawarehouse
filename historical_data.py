f3c3eff450b559366ddc45d3e598174d1aafd1fc
Tom Kenig <Tomaszkenig@Gmail.com>
2021-09-26 22:32:46 +0200

    v0.01 dev

--- historical_data.py
+++ historical_data.py
@@ -48,7 +48,8 @@ stock_type = download_setting[0][3]
 stock_exchange = download_setting[0][4]
 range_to_download = download_setting[0][5]
 download_interval_sec = download_setting[0][6]
-
+daily_update_from_files = download_setting[0][7]
+monthly_update_from_files = download_setting[0][8]
 
 # todo: function get single setting to download: curr, daily, monthly
 
@@ -67,7 +68,6 @@ def get_settings():
 
 
 
-
 DAILY_HISTORY_DAYS = 3
 
 # todo: filenames to download # daily first in dev. Max Last 62 days
@@ -83,6 +83,7 @@ def get_filenames_to_download_daily():
         # print(day)
     return (l)
 
+print(get_filenames_to_download_daily())
 
 FILE_LENGTH = 'daily'
 TYPE = 'klines'
@@ -116,22 +117,22 @@ def get_files():
     for i in csv_data:
         cursor.execute("INSERT INTO " + db_schema_name+"."+db_table_name +"(open_time, open, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume, `ignore`, market, tick_interval, stock_type, stock_exchange) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], market, tick_interval, stock_type, stock_exchange))
         print(i)
-
+    cnxn.commit()
+    cursor.close()
+    cnxn.close()
     print("insert done")
+
 
 
 
 
 FILENAME_LIST = get_filenames_to_download_daily()
 
-for j in FILENAME_LIST:
-    FILENAME = j
-    FILE_PATH = APP_PATH + FILENAME
-    BASE_URL = "https://data.binance.vision/data/"+stock_type+"/"+FILE_LENGTH+"/"+TYPE+"/"+market+"/"+tick_interval+"/"+FILENAME+".zip"
+FILENAME = FILENAME_LIST[0]
+FILE_PATH = APP_PATH + FILENAME
+BASE_URL = "https://data.binance.vision/data/"+stock_type+"/"+FILE_LENGTH+"/"+TYPE+"/"+market+"/"+tick_interval+"/"+FILENAME+".zip"
 
-    print(FILE_PATH)
-    get_files()
+print(BASE_URL)
 
-cnxn.commit()
-cursor.close()
-cnxn.close()
+print(FILE_PATH)
+get_files()
