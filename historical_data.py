228659be78ca4bf4d6a57ef8c3327f0ea08730e1
Tom Kenig <Tomaszkenig@Gmail.com>
2021-09-27 23:37:20 +0200

    v0.01 dev

--- historical_data.py
+++ historical_data.py
@@ -55,6 +55,7 @@ monthly_update_from_files = download_setting[0][8]
 # todo: function get single setting to download: curr, daily, monthly
 
 DAILY_HISTORY_DAYS = 3
+MONTHLY_HISTORY_DAYS = 30
 
 # todo: filenames to download # daily first in dev. Max Last 62 days
 def get_filenames_to_download_daily():
@@ -69,9 +70,24 @@ def get_filenames_to_download_daily():
         # print(day)
     return (l)
 
-print(get_filenames_to_download_daily())
 
-FILE_LENGTH = 'daily'
+def get_filenames_to_download_monthly():
+    # todo: if is records get last + 1, else get all historical from last 62 days. GET ALSO MISSING DAYS <MAYBE OTHER FUNCTION WILL BE BETTER TO DO THIS????
+    start_date = datetime.strptime(str(datetime.utcnow() - timedelta(days=MONTHLY_HISTORY_DAYS))[0:10], "%Y-%m-%d")
+    end_date = datetime.strptime(str(datetime.utcnow() - timedelta(days=0))[0:10], "%Y-%m-%d")
+    delta = end_date - start_date
+    l = []
+    for i in range(delta.days + 1):
+        day = start_date + timedelta(days=i)
+        l.insert(i, "" + market +"-"+ tick_interval +"-"+ str(day)[0:7] +"")
+        # print(day)
+    return list(set(l)) # works like distinct
+
+
+
+print(get_filenames_to_download_daily())
+print(get_filenames_to_download_monthly())
+
 TYPE = 'klines'
 APP_PATH = "tmp/"
 
@@ -107,18 +123,64 @@ def get_daily_files():
     print("insert done")
 
 
-   # start_download_ux_timestamp = 1
-   # start_download_ux_timestamp
+
+def get_monthly_files():
+    # todo: file import
+    r = requests.get(BASE_URL)
+    open(FILE_PATH + ".zip", "wb").write(r.content)
+
+    # todo: zip unpack
+    with zipfile.ZipFile(FILE_PATH + ".zip", "r") as zip_ref:
+        zip_ref.extractall("tmp")
+
+    # insert file into DBMS
+    csv_data = csv.reader(open(FILE_PATH +".csv"))
+    open_time_min = min(csv_data)[0]
+
+    csv_data = csv.reader(open(FILE_PATH +".csv"))
+    open_time_max = max(csv_data)[0]
+
+    csv_data = csv.reader(open(FILE_PATH +".csv"))
+    # delete old data to overwrite
+    cursor.execute(
+        "DELETE FROM " + db_schema_name + "." + db_table_name + " where open_time >= %s and open_time <= %s and tick_interval = %s and market = %s and stock_type = %s and stock_exchange = %s",
+        (open_time_min, open_time_max, tick_interval, market, stock_type, stock_exchange))
+    print("delete done")
+
+    # insert data
+    for i in csv_data:
+        cursor.execute("INSERT INTO " + db_schema_name+"."+db_table_name +"(open_time, open, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume, `ignore`, market, tick_interval, stock_type, stock_exchange) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], market, tick_interval, stock_type, stock_exchange))
+        print(i)
+
+    print("insert done")
 
 
-FILENAME_LIST = get_filenames_to_download_daily()
+
+FILENAME_LIST_DAILY = get_filenames_to_download_daily()
+
+# daily files
+try:
+    for j in FILENAME_LIST_DAILY:
+        FILENAME = j
+        FILE_PATH = APP_PATH + FILENAME
+        BASE_URL = "https://data.binance.vision/data/"+stock_type+"/daily/"+TYPE+"/"+market+"/"+tick_interval+"/"+FILENAME+".zip"
+        get_daily_files()
+except:
+    print("error_1")
 
 
-for j in FILENAME_LIST:
-    FILENAME = FILENAME_LIST[0]
-    FILE_PATH = APP_PATH + FILENAME
-    BASE_URL = "https://data.binance.vision/data/"+stock_type+"/"+FILE_LENGTH+"/"+TYPE+"/"+market+"/"+tick_interval+"/"+FILENAME+".zip"
-    get_daily_files()
+FILENAME_LIST_MONTHLY = get_filenames_to_download_monthly()
+# monthly
+try:
+    for k in FILENAME_LIST_MONTHLY:
+        FILENAME = k
+        FILE_PATH = APP_PATH + FILENAME
+        BASE_URL = "https://data.binance.vision/data/"+stock_type+"/monthly/"+TYPE+"/"+market+"/"+tick_interval+"/"+FILENAME+".zip"
+        get_monthly_files()
+except:
+    print("error_2")
+
+
 
 
 cnxn.commit()
