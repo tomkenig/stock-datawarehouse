fbd64776e173ce814667fd43cd05022c88deee53
Tom Kenig <Tomaszkenig@Gmail.com>
2021-10-02 23:00:12 +0200

    v0.01dev

--- test2.py
+++ test2.py
@@ -69,8 +69,7 @@ start_download_ux_timestamp = download_setting[0][9]
 
 # todo: function get single setting to download: curr, daily, monthly
 
-print(datetime.utcnow().timestamp())
-print(start_download_ux_timestamp)
+
 
 MONTHLY_HISTORY_DAYS = int(datetime.utcnow().timestamp())/86400 - start_download_ux_timestamp/86400
 print(MONTHLY_HISTORY_DAYS)
@@ -87,7 +86,7 @@ def delete_temp_catalog():
 def get_filenames_to_download_monthly():
     # todo: if is records get last + 1, else get all historical from last 62 days. GET ALSO MISSING DAYS <MAYBE OTHER FUNCTION WILL BE BETTER TO DO THIS????
     start_date = datetime.strptime(str(datetime.utcnow() - timedelta(days=MONTHLY_HISTORY_DAYS))[0:10], "%Y-%m-%d")
-    end_date = datetime.strptime(str(datetime.utcnow() - timedelta(days=0))[0:10], "%Y-%m-%d")
+    end_date = datetime.strptime(str(datetime.utcnow() - timedelta(days=1))[0:10], "%Y-%m-%d")
     delta = end_date - start_date
     l = []
     for i in range(delta.days + 1):
@@ -128,18 +127,23 @@ def get_monthly_files():
         print("delete done")
         # insert data
         for i in csv_data:
-            cursor.execute("INSERT INTO " + db_schema_name+"."+db_table_name +"(open_time, open, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume, `ignore`, market, tick_interval, stock_type, stock_exchange) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], market, tick_interval, stock_type, stock_exchange))
-            print(i)
+            cursor.execute("INSERT INTO " + db_schema_name+"."+db_table_name + "(open_time, open, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume, `ignore`, market, tick_interval, stock_type, stock_exchange) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], market, tick_interval, stock_type, stock_exchange))
+            # print(i)
 
-        cnxn.commit()
-        print("insert done")
+        print("insert done: " + FILENAME)
+
+
+        csv_data = csv.reader(open(FILE_PATH +".csv"))
+        new_time_start = min(csv_data)[0]
+
+
         # update settings with new start date
-        print(int(open_time_max/1000))
-        cursor.execute("UPDATE " + db_schema_name + "." + db_settings_table_name + " SET start_download_ux_timestamp = %s where download_settings_id = %s", (int(open_time_max/1000), download_settings_id))
-        print("update done")
+        cursor.execute("UPDATE  " + db_schema_name + "." + db_settings_table_name + " SET start_download_ux_timestamp = %s where download_settings_id = %s", (str(int(int(new_time_start)/1000)), str(download_settings_id)))
+        cnxn.commit()
+        print("update settings done")
 
         # delete files in tmp catalog
-        delete_temp_catalog()
+
 
     except:
         print("err_11: no file probably")
--- test3.py
+++ test3.py
@@ -1,9 +1,49 @@
+
+
+# libs
+# import stockdwh_functions
+import requests
+import mysql.connector  # https://dev.mysql.com/doc/connector-python/en/
+import json
+from datetime import datetime, timedelta
+import math
+import zipfile
 import csv
 
-csv_data = csv.reader(open(FILE_PATH + ".csv"))
-open_time_min = min(csv_data)[0]
+# get database connection credentials from JSON file
+with open('db_credentials.json') as json_conf:
+    sql_db_conn = (json.load(json_conf))
 
-csv_data = csv.reader(open(FILE_PATH + ".csv"))
-open_time_max = max(csv_data)[0]
+# db driver (MySQL) connection settings
+cnxn = mysql.connector.connect(user=sql_db_conn["user"],
+                               password=sql_db_conn["password"],
+                               host=sql_db_conn["host"],
+                               database=sql_db_conn["database"])
 
-csv_data = csv.reader(open(FILE_PATH + ".csv"))
+# destination
+db_schema_name = sql_db_conn["db_schema_name"]  # schema_name
+db_table_name = sql_db_conn["db_table_name"]  # table_name
+db_settings_table_name = sql_db_conn["db_settings_table_name"]  # settings table name
+
+# open db connection
+cursor = cnxn.cursor()
+
+cursor.execute("SELECT download_settings_id, market, tick_interval, stock_type, stock_exchange, current_range_to_overwrite, download_interval_sec, daily_update_from_files, monthly_update_from_files, start_download_ux_timestamp FROM " + db_schema_name + "." + db_settings_table_name + " WHERE daily_update_from_files = 1 and daily_hist_complete = 0 order by start_download_ux_timestamp asc limit 1")
+download_setting = cursor.fetchall()
+download_settings_id = download_setting[0][0]
+market = download_setting[0][1]
+tick_interval = download_setting[0][2]
+stock_type = download_setting[0][3]
+stock_exchange = download_setting[0][4]
+range_to_download = download_setting[0][5]
+download_interval_sec = download_setting[0][6]
+daily_update_from_files = download_setting[0][7]
+monthly_update_from_files = download_setting[0][8]
+start_download_ux_timestamp = download_setting[0][9]
+
+
+print(download_setting)
+print(download_settings_id)
+
+cursor.execute("UPDATE  " + db_schema_name + "." + db_settings_table_name + " SET start_download_ux_timestamp = %s where download_settings_id = %s", (str(999000), str(download_settings_id)))
+cnxn.commit()
